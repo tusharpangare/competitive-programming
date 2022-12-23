a2e5b25b83ee7efe59aeca7f25abdb19261590a5
tusharpangare <tusharpangare99@gmail.com>
2022-12-23 11:25:35 +0530

    day5 codes

---
+++ codechef/prime_reversal.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    a=input()
+    b=input()
+    if a.count("0")==b.count("0"):
+        print("YES")
+    else:
+        print("NO")    
