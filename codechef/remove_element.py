fcdceb940a49cf6f25d6ef4610480750c936f84c
tusharpangare <tusharpangare99@gmail.com>
2023-06-09 10:34:08 +0530

    code added for remove_element codechef

---
+++ codechef/remove_element.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    arr=list(map(int,input().split()[:n]))
+    arr.sort()
+    while len(arr)>1:
+        if arr[0]+arr[-1]<=k:
+            arr.pop()
+        else:
+            break
+
+    print("Yes") if len(arr)==1 else print("No")
+
+
+
+    
